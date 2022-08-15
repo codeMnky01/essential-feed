d3b1ad648f5bb73f32990ad04a87c741068cddc6
Andrey Negovskiy <a.negovskiy@gmail.com>
2022-08-15 17:16:57 +0300

    Add `RemoteFeedItem` data transfer representation to decouple `FeedItemsMapper` from `FeedItem` data model

--- EssentialFeed/EssentialFeed/Feed API/FeedItemsMapper.swift
+++ EssentialFeed/EssentialFeed/Feed API/FeedItemsMapper.swift
@@ -7,40 +7,28 @@
 
 import Foundation
 
+internal struct RemoteFeedItem: Decodable {
+    internal let id: UUID
+    internal let description: String?
+    internal let location: String?
+    internal let image: URL
+}
+
 internal final class FeedItemsMapper {
     private struct Root: Decodable {
-        let items: [Item]
-        
-        var feed: [FeedItem] {
-            items.map { $0.item }
-        }
+        let items: [RemoteFeedItem]
     }
 
-    private struct Item: Equatable, Decodable {
-        let id: UUID
-        let description: String?
-        let location: String?
-        let image: URL
-        
-        var item: FeedItem {
-            FeedItem(
-                id: id,
-                description: description,
-                location: location,
-                imageURL: image)
-        }
-    }
-    
     private static var OK_200: Int { 200 }
     
-    internal static func map(response: HTTPURLResponse, data: Data) -> RemoteFeedLoader.Result {
+    internal static func map(response: HTTPURLResponse, data: Data) throws -> [RemoteFeedItem] {
         guard
             response.statusCode == OK_200,
             let root = try? JSONDecoder().decode(Root.self, from: data)
         else {
-            return .failure(RemoteFeedLoader.Error.invalidData)
+            throw RemoteFeedLoader.Error.invalidData
         }
         
-        return (.success(root.feed))
+        return root.items
     }
 }
