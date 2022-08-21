1475fd12fb68b9862d1047088e60aea0b3d98049
Andrey Negovskiy <a.negovskiy@gmail.com>
2022-08-21 15:53:43 +0300

    Separate CoreData managed object classes from helper methods with extension

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedFeedImage.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedFeedImage.swift
@@ -14,7 +14,9 @@ class ManagedFeedImage: NSManagedObject {
     @NSManaged var location: String?
     @NSManaged var url: URL
     @NSManaged var cache: ManagedCache
-    
+}
+
+extension ManagedFeedImage {
     static func images(from feed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
         NSOrderedSet(array: feed.map { local in
             let managed = ManagedFeedImage(context: context)
