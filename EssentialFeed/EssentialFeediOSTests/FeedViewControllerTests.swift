b0bdcac6f408183e9a280d41773943aad66389ea
Andrey Negovskiy <a.negovskiy@gmail.com>
2022-08-25 14:54:41 +0300

    Does not load feed on init (before the view is loaded)

---
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -0,0 +1,31 @@
+//
+//  FeedViewController.swift
+//  EssentialFeediOSTests
+//
+//  Created by Andrey on 8/25/22.
+//
+
+import XCTest
+
+final class FeedViewController {
+    init(loader: FeedViewControllerTests.LoaderSpy) {
+        
+    }
+}
+
+final class FeedViewControllerTests: XCTestCase {
+    
+    func test_init_doesNotLoadFeed() {
+        let loader = LoaderSpy()
+        let _ = FeedViewController(loader: loader)
+        
+        XCTAssertEqual(loader.loadCallCount, 0)
+    }
+    
+    // MARK: - Helpers
+    
+    class LoaderSpy {
+        private(set) var loadCallCount = 0
+    }
+    
+}
