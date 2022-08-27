49d93a0331d2471676bc39651862bb85fdc1002d
Andrey Negovskiy <a.negovskiy@gmail.com>
2022-08-27 19:29:38 +0300

    Extract `FeedViewControllerTests` `LoaderSpy` into a separate file

--- EssentialFeed/EssentialFeediOSTests/Feed UI/Controllers/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/Feed UI/Controllers/FeedViewControllerTests.swift
@@ -259,56 +259,6 @@ final class FeedViewControllerTests: XCTestCase {
     func makeImage(description: String? = nil, location: String? = nil, url: URL = URL(string: "any-url.com")!) -> FeedImage {
         FeedImage(id: UUID(), description: description, location: location, url: url)
     }
-    
-    class LoaderSpy: FeedLoader, FeedImageDataLoader {
-        
-        // MARK: - FeedLoader
-        private var feedRequests = [(FeedLoader.Result) -> Void]()
-        var loadFeedCallCount: Int { feedRequests.count }
-        
-        func load(completion: @escaping (FeedLoader.Result) -> Void) {
-            feedRequests.append(completion)
-        }
-        
-        func completeFeedLoading(with feed: [FeedImage] = [], at index: Int) {
-            feedRequests[index](.success(feed))
-        }
-        
-        func completeFeedLoadingWithError(at index: Int) {
-            let error = NSError(domain: "domain", code: 1)
-            feedRequests[index](.failure(error))
-        }
-        
-        // MARK: - FeedImageDataLoader
-        var loadedImageURLs: [URL] {
-            imageRequests.map(\.url)
-        }
-        
-        private(set) var canceledImageURLs = [URL]()
-        private(set) var imageRequests = [(url: URL, completion: (FeedImageDataLoader.Result) -> Void)]()
-        
-        private struct TaskSpy: FeedImageDataLoaderTask {
-            let cancelCallback: () -> Void
-            
-            func cancel() {
-                cancelCallback()
-            }
-        }
-        
-        func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-            imageRequests.append((url, completion))
-            return TaskSpy { [weak self] in self?.canceledImageURLs.append(url) }
-        }
-        
-        func completeImageDataLoading(with data: Data = Data(), at index: Int) {
-            imageRequests[index].completion(.success(data))
-        }
-        
-        func completeImageDataLoadingWithError(at index: Int) {
-            let error = NSError(domain: "domain", code: 1)
-            imageRequests[index].completion(.failure(error))
-        }
-    }
 }
 extension UIButton {
     func simulateTap() {
