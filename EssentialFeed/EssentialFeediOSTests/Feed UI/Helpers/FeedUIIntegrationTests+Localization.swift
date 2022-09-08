852fa382f91b63402fee79db8dbb5c28d3c5d3c9
Andrey Negovskiy <a.negovskiy@gmail.com>
2022-09-08 13:20:09 +0300

    Replace `FeedPresenter` from iOS module with the `FeedPresenter` from the cross platform `EssentialFeed` module

--- EssentialFeed/EssentialFeediOSTests/Feed UI/Helpers/FeedUIIntegrationTests+Localization.swift
+++ EssentialFeed/EssentialFeediOSTests/Feed UI/Helpers/FeedUIIntegrationTests+Localization.swift
@@ -6,12 +6,12 @@
 //
 
 import XCTest
-import EssentialFeediOS
+import EssentialFeed
 
 extension FeedUIIntegrationTests {
     func localized(_ key: String, file: StaticString = #filePath, line: UInt = #line) -> String {
         let table = "Feed"
-        let bundle = Bundle(for: FeedViewController.self)
+        let bundle = Bundle(for: FeedPresenter.self)
         let localizedValue = bundle.localizedString(forKey: key, value: nil, table: table)
         
         if localizedValue == key {
