75516dd69a703d2f4b4dca894e4d8d84844dbe2b
Andrey Negovskiy <a.negovskiy@gmail.com>
2022-09-08 15:55:55 +0300

    `FeedImagePresenter` displays image on successful load

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -8,10 +8,10 @@
 import XCTest
 import EssentialFeed
 
-struct FeedImageViewModel {
+struct FeedImageViewModel<Image> {
     let description: String?
     let location: String?
-    let image: Any?
+    let image: Image?
     let isLoading: Bool
     let shouldRetry: Bool
     
@@ -21,14 +21,16 @@ struct FeedImageViewModel {
 }
 
 protocol FeedImageView {
-    func display(_ viewModel: FeedImageViewModel)
+    associatedtype Image
+    
+    func display(_ viewModel: FeedImageViewModel<Image>)
 }
 
-final class FeedImagePresenter {
-    private let view: FeedImageView
-    private var imageTransformer: (Data) -> Any?
+final class FeedImagePresenter<View: FeedImageView, Image> where View.Image == Image {
+    private let view: View
+    private var imageTransformer: (Data) -> Image?
     
-    init(view: FeedImageView, transformer: @escaping (Data) -> Any?) {
+    init(view: View, transformer: @escaping (Data) -> Image?) {
         self.view = view
         self.imageTransformer = transformer
     }
@@ -45,9 +47,16 @@ final class FeedImagePresenter {
     private struct InvalidDataImageError: Error {}
     
     func didFinishImageDataLoadingWith(data: Data, for model: FeedImage) {
-        guard let _ = imageTransformer(data) else {
+        guard let image = imageTransformer(data) else {
             return didFinishImageDataLoadingWith(error: InvalidDataImageError(), for: model)
         }
+        
+        view.display(FeedImageViewModel(
+            description: model.description,
+            location: model.location,
+            image: image,
+            isLoading: false,
+            shouldRetry: false))
     }
     
     func didFinishImageDataLoadingWith(error: Error, for model: FeedImage) {
@@ -98,13 +107,29 @@ class FeedImagePresenterTests: XCTestCase {
         XCTAssertNil(message?.image)
     }
     
+    func test_didFinishImageDataLoading_displayImage() {
+        let (sut, view) = makeSUT { _ in AnyImage() }
+        let data = Data()
+        let feedImage = uniqueImage()
+        
+        sut.didFinishImageDataLoadingWith(data: data, for: feedImage)
+        
+        let message = view.messages.first
+        XCTAssertEqual(view.messages.count, 1)
+        XCTAssertEqual(message?.description, feedImage.description)
+        XCTAssertEqual(message?.location, feedImage.location)
+        XCTAssertEqual(message?.isLoading, false)
+        XCTAssertEqual(message?.shouldRetry, false)
+        XCTAssertNotNil(message?.image)
+    }
+    
     // MARK: - Helpers
     
-    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: FeedImagePresenter, view: ViewSpy) {
+    private func makeSUT(_ imageTransformer: @escaping (Data) -> AnyImage? = { _ in nil }, file: StaticString = #filePath, line: UInt = #line) -> (sut: FeedImagePresenter<ViewSpy, AnyImage>, view: ViewSpy) {
         let view = ViewSpy()
         let sut = FeedImagePresenter(
             view: view,
-            transformer: alwaysFailingImageTransformer)
+            transformer: imageTransformer)
         
         trackMemoryLeaks(instance: view, file: file, line: line)
         trackMemoryLeaks(instance: sut, file: file, line: line)
@@ -112,15 +137,17 @@ class FeedImagePresenterTests: XCTestCase {
         return (sut, view)
     }
     
+    private struct AnyImage: Equatable {}
+    
     private class ViewSpy: FeedImageView {
-        private(set) var messages = [FeedImageViewModel]()
+        private(set) var messages = [FeedImageViewModel<AnyImage>]()
         
-        func display(_ viewModel: FeedImageViewModel) {
+        func display(_ viewModel: FeedImageViewModel<AnyImage>) {
             messages.append(viewModel)
         }
     }
     
-    private var alwaysFailingImageTransformer: (Data) -> Any? {
+    private var alwaysFailingImageTransformer: (Data) -> AnyImage? {
         { _ in nil }
     }
 }
