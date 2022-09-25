9336d0442618fae36957e3a64f8096c04a075b58
Andrey Negovskiy <a.negovskiy@gmail.com>
2022-09-25 18:12:58 +0300

    Add algorithm that allows to compare snapshot with tolerance (to avoid M1 vs Intel issues)

--- EssentialFeed/EssentialFeediOSTests/Helpers/XCTestCase+Snapshot.swift
+++ EssentialFeed/EssentialFeediOSTests/Helpers/XCTestCase+Snapshot.swift
@@ -9,7 +9,10 @@ import XCTest
 
 extension XCTest {
     func assert(_ snapshot: UIImage, name: String, file: StaticString = #filePath, line: UInt = #line) {
-        let snapshotData = makeSnapshotData(snapshot, file: file, line: line)
+        guard let snapshotData = makeSnapshotData(snapshot, file: file, line: line) else {
+            return
+        }
+        
         let snapshotURL = makeSnapshotURL(name, file: file, line: line)
         
         guard let storedSnapshotData = try? Data(contentsOf: snapshotURL) else {
@@ -17,10 +20,10 @@ extension XCTest {
             return
         }
         
-        if snapshotData != storedSnapshotData {
+        if !match(snapshotData, storedSnapshotData, tolerance: 0.00001) {
             let temporarySnapshotURL = URL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true).appendingPathComponent(snapshotURL.lastPathComponent)
             
-            try? snapshotData?.write(to: temporarySnapshotURL)
+            try? snapshotData.write(to: temporarySnapshotURL)
             
             XCTFail("Snapshots does not match. Stored: \(snapshotURL), New: \(temporarySnapshotURL)", file: file, line: line)
         }
@@ -54,5 +57,66 @@ extension XCTest {
             .deletingLastPathComponent()
             .appendingPathComponent("snapshots")
             .appendingPathComponent("\(name).png")
+    }
+    
+    private func match(_ oldData: Data, _ newData: Data, tolerance: Float = 0) -> Bool {
+        if oldData == newData { return true }
+        
+        guard let oldImage = UIImage(data: oldData)?.cgImage, let newImage = UIImage(data: newData)?.cgImage else {
+            return false
+        }
+        
+        guard oldImage.width == newImage.width, oldImage.height == newImage.height else {
+            return false
+        }
+        
+        let minBytesPerRow = min(oldImage.bytesPerRow, newImage.bytesPerRow)
+        let bytesCount = minBytesPerRow * oldImage.height
+        
+        var oldImageByteBuffer = [UInt8](repeating: 0, count: bytesCount)
+        guard let oldImageData = data(for: oldImage, bytesPerRow: minBytesPerRow, buffer: &oldImageByteBuffer) else {
+            return false
+        }
+        
+        var newImageByteBuffer = [UInt8](repeating: 0, count: bytesCount)
+        guard let newImageData = data(for: newImage, bytesPerRow: minBytesPerRow, buffer: &newImageByteBuffer) else {
+            return false
+        }
+        
+        if memcmp(oldImageData, newImageData, bytesCount) == 0 { return true }
+        
+        return match(oldImageByteBuffer, newImageByteBuffer, tolerance: tolerance, bytesCount: bytesCount)
+    }
+    
+    private func data(for image: CGImage, bytesPerRow: Int, buffer: UnsafeMutableRawPointer) -> UnsafeMutableRawPointer? {
+        guard
+            let space = image.colorSpace,
+            let context = CGContext(
+                data: buffer,
+                width: image.width,
+                height: image.height,
+                bitsPerComponent: image.bitsPerComponent,
+                bytesPerRow: bytesPerRow,
+                space: space,
+                bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue
+            )
+        else { return nil }
+        
+        context.draw(image, in: CGRect(x: 0, y: 0, width: image.width, height: image.height))
+        
+        return context.data
+    }
+    
+    private func match(_ bytes1: [UInt8], _ bytes2: [UInt8], tolerance: Float, bytesCount: Int) -> Bool {
+        var differentBytesCount = 0
+        for i in 0 ..< bytesCount where bytes1[i] != bytes2[i] {
+            differentBytesCount += 1
+            
+            let percentage = Float(differentBytesCount) / Float(bytesCount)
+            if percentage > tolerance {
+                return false
+            }
+        }
+        return true
     }
 }
